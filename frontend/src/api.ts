4e8e2a7450bc62088ba9f69c1b14b1ff02944700
kegmor <kegmor07@gmail.com>
2026-03-31 19:30:32 -0500

    feat: created api client

---
+++ frontend/src/api.ts
@@ -0,0 +1,61 @@
+const API_URL = import.meta.env.VITE_API_URL
+const API_KEY = import.meta.env.VITE_API_KEY
+
+const headers = {
+    'Content-type': 'application/json',
+    'x-api-key': API_KEY,
+}
+
+export async function getSets() {
+    const response = await fetch(`${API_URL}/sets`, { headers });
+    if(!response.ok) throw new Error('Failed to fetch sets');
+    return response.json();
+}
+
+export async function getSetsById(id: string) {
+    const response = await fetch(`${API_URL}/sets/${id}`, { headers });
+    if(!response.ok) throw new Error('Failed to fetch set by id');
+    return response.json();
+}
+
+export async function createSet(url: string, djName: string) {
+    const data = {
+        'url': url,
+        'dj_name': djName
+    }
+    const response = await fetch(`${API_URL}/sets`, { 
+        method: 'POST', 
+        headers, 
+        body: JSON.stringify(data) 
+    });
+    if(!response.ok) throw new Error('Failed to create set');
+    return response.json();
+}
+
+export async function deleteSetById(id: string) {
+    const response = await fetch(`${API_URL}/sets/${id}`, { 
+        method: 'DELETE',
+        headers
+    });
+    if(!response.ok) throw new Error('Failed to delete set');
+    return response.json();
+}
+
+export async function getCategories() {
+    const response = await fetch(`${API_URL}/categories`, { headers });
+    if(!response.ok) throw new Error('Failed to fetch categories');
+    return response.json();
+}
+
+export async function createCategory(name: string) {
+    const data = {
+        'name': name
+    }
+    const response = await fetch(`${API_URL}/categories`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data)
+    });
+    if(!response.ok) throw new Error('Failed to create category');
+    return response.json();
+}

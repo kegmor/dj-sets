f63ab472b28529443b913ef0fd8e68f3462cbdaf
kegmor <kegmor07@gmail.com>
2026-04-03 11:00:01 -0500

    feat: Added create set functionality with form and submit handler

--- frontend/src/pages/AddSet.tsx
+++ frontend/src/pages/AddSet.tsx
@@ -1,4 +1,36 @@
+import { useState } from "react";
+import { createSet } from "../api";
+import { useNavigate } from 'react-router-dom'
+
 function AddSet() {
-    return <h1>AddSet</h1>
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState ({
+        url: "",
+        djName: ""
+    })
+    
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = async () => {
+        await createSet(formData.url, formData.djName);
+        navigate('/');
+    }
+
+    return (
+        <form>
+            <div>
+                <label>URL</label>
+                <input type="text" name="url" onChange={handleInput}/>
+            </div>
+            <div>
+                <label>DJ Name</label>
+                <input type="text" name="djName" onChange={handleInput}/>
+            </div>
+            <button type="button" onClick={handleSubmit}>Add Set</button>
+        </form>
+    )
 }
 export default AddSet;
